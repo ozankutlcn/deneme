6f3a2938500036bf9bbf3f17a35b0916bc1c4cce
ozan_kutlucan_@hotmail.com <ozan_kutlucan_@hotmail.com>
2023-01-20 15:37:42 +0300

    deneme2

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                ID = 1,
+                FirstName = "Ozan",
+                LastName = "Kutlucan",
+                Address = "Akşemsettin"
+            };
+            
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson 
+    {
+        int ID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;  }
+        public string Address { get; set; }
+
+    }
+    class Student : IPerson
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    //class PersonManager 
+    //{
+    //    public void Add(IPerson person) 
+    //    {
+    //        Console.WriteLine(customer.FirstName);
+    //    }
+    //}
+}
